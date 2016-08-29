0485494776d4df8553c330c52d837d23de38b2f2
wellyponka <pmashiro@mail.piwi.com.tw>
2016-08-29 17:56:54 +0800

    [add] Account delete & edit

--- src/main/java/com/SBS/mapper/AccountMapper.java
+++ src/main/java/com/SBS/mapper/AccountMapper.java
@@ -2,10 +2,12 @@ package com.SBS.mapper;
 
 import java.util.List;
 
+import org.apache.ibatis.annotations.Delete;
 import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
 import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.annotations.Update;
 
 import com.SBS.bean.Account;
 
@@ -20,4 +22,10 @@ public interface AccountMapper {
 	
 	@Insert("insert into Account (name, pwd, role) values (#{name}, #{pwd}, #{role})")
 	int registerUser(Account acc);
+	
+	@Delete("DELETE FROM Account WHERE name=#{name}")
+	int removeUser(Account acc);
+	
+	@Update("UPDATE Account SET pwd = #{pwd} WHERE name = #{name}")
+	int editUser(Account acc);
 }
--- src/main/java/com/SBS/web/WebAccountCRUD.java
+++ src/main/java/com/SBS/web/WebAccountCRUD.java
@@ -3,6 +3,7 @@ package com.SBS.web;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -32,6 +33,26 @@ public class WebAccountCRUD {
 	public Result<Account> getAllAccount() {
 		List<Account> acc = this.accMapper.getAllAccount();
 		return new Result<Account>().setError(0).setMsg("Register success!!").setData(acc);
+		
+	}
+	
+	@RequestMapping(value = "/WebRemoveAccount", method = RequestMethod.POST)
+	public Result<?> removeAccount(@RequestParam String name) {
+		Account acc = new Account().setName(name);
+		if(this.accMapper.removeUser(acc) == 1)
+			return new Result<Object>().setError(0).setMsg("Remove user success!!");
+		else
+			return new Result<Object>().setError(-1).setMsg("Remove user fail QQ");
+		
+	}
+	
+	@RequestMapping(value = "/WebEditAccount", method = RequestMethod.POST)
+	public Result<?> editAccount(@RequestParam String name, String pwd) {
+		Account acc = new Account().setName(name).setPwd(pwd);
+		if(this.accMapper.editUser(acc) == 1)
+			return new Result<Object>().setError(0).setMsg("Edit user success!!");
+		else
+			return new Result<Object>().setError(-1).setMsg("Edit user fail QQ");
 		
 	}
 }
